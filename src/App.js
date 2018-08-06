5f967534daed7fe973630f098e67c50489549b29
Emre Gonen <emre.gonen@asyasoft.net>
2018-08-06 16:18:05 +0300

    Added apollo provider & appsync client

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 import Nav from './navigation/Nav'
-import Amplify from 'aws-amplify';
+import Amplify, {Auth} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native'
+import {Rehydrated} from 'aws-appsync-react'
+import {ApolloProvider} from 'react-apollo'
+import AWSAppSyncClient from 'aws-appsync'
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
+
+const client = new AWSAppSyncClient({
+    url: aws_exports.graphqlEndpoint,
+    region: aws_exports.region,
+    auth: {
+        type: aws_exports.authenticationType,
+        jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
+    }
+});
 
 class App extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Nav/>
-            </View>
+            <ApolloProvider client={client}>
+                <Rehydrated>
+                    <Nav/>
+                </Rehydrated>
+            </ApolloProvider>
         );
     }
 }
